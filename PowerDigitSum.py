689b7f3aa61d8aa0ecd3afd2e0db3619e1997fa1
ConradPoonWebb <76155770+ConradPoonWebb@users.noreply.github.com>
2021-01-10 17:59:50 +0800

    Added some comments

--- PowerDigitSum.py
+++ PowerDigitSum.py
@@ -4,11 +4,11 @@ do 2**1000 and use a for loop to add'''
 
 theNumber = 2**1000
 
-def digitSum(num):
+def digitSum(num): #Adds each digit of 2 to the power of 1000 to a number and returns it
     theSum = 0
     for digit in str(num):
         theSum += int(digit)
     return theSum
 
 total = digitSum(theNumber)
-print ('The sum of all digits in 2 to the power of 1000 is '+ str(total))
+print ('The sum of all digits in 2 to the power of 1000 is '+ str(total)) #Formatting the solution
